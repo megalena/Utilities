7a3a5714c51f71b984fc7324b9bee3802d940148
Elena <elenamanushyna@gmail.com>
2015-02-28 13:45:00 +0200

    The utility to clean all files' Title tags in the selected folder and all its subfolders.

---
+++ CleanFileTitles/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanFileTitles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No folder path provided.");
+                return;
+            }
+
+            var parentFolderPath = args[0];
+            if (!Directory.Exists(parentFolderPath))
+            {
+                Console.WriteLine("The folder path is not valid.");
+                return;
+            }
+
+            CleanFileTitles(parentFolderPath);
+            Console.WriteLine("Titles were cleaned successfully.");
+        }
+
+        private static void CleanFileTitles(string parentFolderPath)
+        {
+            foreach (var file in Directory.GetFiles(parentFolderPath))
+            {
+                using (var tagFile = TagLib.File.Create(file))
+                {
+                    tagFile.Tag.Title = String.Empty;
+                    tagFile.Save();
+                }
+            }
+
+            foreach (var folder in Directory.GetDirectories(parentFolderPath))
+            {
+                CleanFileTitles(folder);
+            }
+        }
+    }
+}
